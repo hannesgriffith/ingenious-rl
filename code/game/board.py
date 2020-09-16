fix scores calculation + other bits

code/game/board.py
@@ -1,7 +1,7 @@
 from numba import njit, prange
 import numpy as np
 
-from game.utils import fast_initialise_playable, fast_initialise_start_playable, fast_initialise_areas
+from game.game_utils import fast_initialise_playable, fast_initialise_start_playable, fast_initialise_areas
 
 class Board:
     def __init__(self):
@@ -26,7 +26,6 @@ class Board:
 
     def initialise_available(self):
         self.available = np.zeros((self.height + 2, self.width + 4), dtype=np.uint8)
-        self.last_available = np.zeros((self.height + 2, self.width + 4), dtype=np.uint8)
         self.update_available_for_hexes(self.start_hexes)
 
     def update_available_for_hexes(self, hexes):
@@ -196,41 +195,43 @@ def fast_remove_duplicates(moves):
 
 @njit(cache=True)
 def fast_update_playable_for_hex(playable, coords, neighbour_offsets):
-    i1, j1 = coords
-    playable[i1, j1] = 0
+    i0, j0 = coords
+    playable[i0, j0] = 0
 
     for idx_1 in range(6):
         di1, dj1 = neighbour_offsets[idx_1]
-        i2 = i1 + di1
-        j2 = j1 + dj1
-        if playable[i2, j2] == 0:
-            continue
+        i1 = i0 + di1
+        j1 = j0 + dj1
 
-        has_neighbours = False
-        for idx_2 in range(6):
-            di2, dj2 = neighbour_offsets[idx_2]
-            i3 = i2 + di2
-            j3 = j2 + dj2
-            if playable[i3, j3] == 1:
-                has_neighbours = True
-                break
+        if playable[i1, j1] == 1:
+            has_neighbours = False
 
-        if not has_neighbours:
-            playable[i2, j2] = 0
+            for idx_2 in range(6):
+                di2, dj2 = neighbour_offsets[idx_2]
+                i2 = i1 + di2
+                j2 = j1 + dj2
+                if playable[i2, j2] == 1:
+                    has_neighbours = True
+                    break
+
+            if not has_neighbours:
+                playable[i1, j1] = 0
 
     return playable
 
 @njit(cache=True)
-def fast_update_available_for_hex(available, coords, neighbour_offsets, playable):
-    i1, j1 = coords
-    available[i1, j1] = 0
+def fast_update_available_for_hex(available, coords, offsets, playable):
+    i0, j0 = coords
+    available[i0, j0] = 0
 
-    for o_idx in range(6):
-        di, dj = neighbour_offsets[o_idx]
-        i2 = i1 + di
-        j2 = j1 + dj
-        if playable[i2, j2] == 1:
-            available[i2, j2] = 1
+    for o in range(6):
+        di, dj = offsets[o]
+        i1 = i0 + di
+        j1 = j0 + dj
+        if playable[i1, j1] == 1:
+            available[i1, j1] = 1
+        else:
+            available[i1, j1] = 0
 
     return available
 
@@ -293,7 +294,7 @@ def fast_batch_get_updated_playable(moves, height, width, playable, neighbour_of
     for move_idx in prange(num_moves):
         coords1 = moves[move_idx, 0:2]
         coords2 = moves[move_idx, 2:4]
-        updated_playables[move_idx] = playable
+        updated_playables[move_idx] = playable[:]
         updated_playables[move_idx] = fast_update_playable_for_hex(updated_playables[move_idx], coords1, neighbour_offsets)
         updated_playables[move_idx] = fast_update_playable_for_hex(updated_playables[move_idx], coords2, neighbour_offsets)
 
@@ -307,7 +308,7 @@ def fast_batch_get_updated_available(moves, updated_playables, height, width, av
     for move_idx in prange(num_moves):
         coords1 = moves[move_idx, 0:2]
         coords2 = moves[move_idx, 2:4]
-        updated_availables[move_idx] = available
+        updated_availables[move_idx] = available[:]
         updated_playable = updated_playables[move_idx]
         updated_availables[move_idx] = fast_update_available_for_hex(updated_availables[move_idx], coords1, neighbour_offsets, updated_playable)
         updated_availables[move_idx] = fast_update_available_for_hex(updated_availables[move_idx], coords2, neighbour_offsets, updated_playable)
@@ -320,7 +321,7 @@ def fast_batch_get_updated_states(moves, state, height, width, updated_playables
     updated_states = np.zeros((num_moves, height + 2, width + 4, 11), dtype=np.uint8)
 
     for move_idx in prange(num_moves):
-        updated_states[move_idx, :, :, :9] = state
+        updated_states[move_idx, :, :, :9] = state[:]
 
         coords1 = moves[move_idx, 0:2]
         colour1 = moves[move_idx, 4]
@@ -509,8 +510,7 @@ def fast_update_scoring_arrays_for_hex(coords, colour, clusters, sizes, scores, 
     for i in range(1, height + 1):
         for j in range(2, width + 2):
             if playable[i, j] == 1:
-                for c in range(6):
-                    scores[i, j, 3, c] = scores[i, j, 0, c] + scores[i, j, 1, c] + scores[i, j, 2, c]
+                scores[i, j, 3, colour] = scores[i, j, 0, colour] + scores[i, j, 1, colour] + scores[i, j, 2, colour]
             else:
                 scores[i, j, :, :] = 0
 
@@ -530,9 +530,9 @@ def fast_batch_get_updated_scoring_arrays(moves, updated_playables, height, widt
         colour2 = moves[move_idx, 5]
 
         playable = updated_playables[move_idx]
-        updated_clusters[move_idx] = clusters_original
-        updated_sizes[move_idx] = sizes_original
-        updated_scores[move_idx] = scores_original
+        updated_clusters[move_idx] = clusters_original[:]
+        updated_sizes[move_idx] = sizes_original[:]
+        updated_scores[move_idx] = scores_original[:]
 
         clusters = updated_clusters[move_idx]
         sizes = updated_sizes[move_idx]
@@ -628,8 +628,7 @@ def fast_batch_get_updated_scoring_arrays(moves, updated_playables, height, widt
             for i in range(1, height + 1):
                 for j in range(2, width + 2):
                     if playable[i, j] == 1:
-                        for c in range(6):
-                            scores[i, j, 3, c] = scores[i, j, 0, c] + scores[i, j, 1, c] + scores[i, j, 2, c]
+                        scores[i, j, 3, colour] = scores[i, j, 0, colour] + scores[i, j, 1, colour] + scores[i, j, 2, colour]
                     else:
                         scores[i, j, :, :] = 0
 
@@ -661,7 +660,6 @@ def fast_generate_vector_representation(playable, available, state, scores, heig
         colour_counts_sum = 0
         score_counts_sum_0 = 0
         score_counts_sum_1 = 0
-        score_counts_sum_2 = 0
 
         for i in range(1, height + 1):
             for j in range(2, width + 2):
@@ -671,13 +669,12 @@ def fast_generate_vector_representation(playable, available, state, scores, heig
                 if scores[i, j, 3, c] > 0:
                     score_counts_sum_0 += 1
 
-                if available[i, j] == 1 and scores[i, j, 3, c] == 0:
-                    score_counts_sum_2 += 1
-
         colour_counts[c] = colour_counts_sum
         score_counts[0, c] = score_counts_sum_0
         score_counts[1, c] = score_counts_sum_1
-        score_counts[2, c] = score_counts_sum_2
+
+        if score_counts_sum_0 == 0:
+            score_counts[2, c] = 1
 
         score_counts[3:, c] = fast_count_scores(scores[:, :, 3, c], 8)
 
@@ -714,7 +711,6 @@ def fast_batch_generate_vector_representation(updated_playable, updated_availabl
             colour_counts_sum = 0
             score_counts_sum_0 = 0
             score_counts_sum_1 = 0
-            score_counts_sum_2 = 0
 
             for i in range(1, height + 1):
                 for j in range(2, width + 2):
@@ -724,13 +720,12 @@ def fast_batch_generate_vector_representation(updated_playable, updated_availabl
                     if scores[i, j, 3, c] > 0:
                         score_counts_sum_0 += 1
 
-                    if available[i, j] == 1 and scores[i, j, 3, c] == 0:
-                        score_counts_sum_2 += 1
-
             colour_counts[c] = colour_counts_sum
             score_counts[0, c] = score_counts_sum_0
             score_counts[1, c] = score_counts_sum_1
-            score_counts[2, c] = score_counts_sum_2
+
+            if score_counts_sum_0 == 0:
+                score_counts[2, c] = 1
 
             colour_channel_flat = scores[:, :, 3, c].flatten()
             colour_channel_flat.sort()

code/learn/network.py
@@ -26,7 +26,7 @@ class MLP(nn.Module):
     def __init__(self):
         super().__init__()
         self.num_input_channels = input_channels["vector"]
-        self.num_hidden_units = 64
+        self.num_hidden_units = 128
 
         self.mlp = nn.Sequential(
             nn.Linear(self.num_input_channels, self.num_hidden_units),

code/learn/representation.py
@@ -119,7 +119,6 @@ class RepresentationGenerator:
 
         return new_reprs_buffer, possible_moves_subset
 
-# @njit(cache=True)
 def fast_generate(board_repr1, board_repr2, board_vec, deck_repr, your_score_repr, other_score_repr, ingenious, num_ingenious, can_exchange, should_exchange, move_num):
     scores_repr = np.vstack((
         np.expand_dims(your_score_repr, 0),
@@ -228,6 +227,23 @@ def fast_augment(board_repr1, board_repr2, board_vec, deck_repr, scores_repr, ge
 
     return (board_repr1, board_repr2, board_vec, deck_repr, scores_repr, general_repr)
 
+def fast_random_transformations(board_repr1, board_repr2, board_vec, deck_repr, scores_repr, general_repr):
+    flip = np.random.randint(0, high=2, size=3)
+
+    if flip[0]:
+        board_repr1[:, ::-1, :, :]
+        board_repr2[:, ::-1, :, :, :]
+
+    if flip[1]:
+        board_repr1[:, :, ::-1, :]
+        board_repr2[:, :, ::-1, :, :]
+
+    if flip[2]:
+        board_repr1 = np.rot90(board_repr1, k=2, axes=(1, 2))
+        board_repr2 = np.rot90(board_repr2, k=2, axes=(1, 2))
+
+    return (board_repr1, board_repr2, board_vec, deck_repr, scores_repr, general_repr)
+
 @njit(parallel=True, fastmath=True, cache=True)
 def fast_normalise(board_repr1, board_repr2, board_vec, deck_repr, scores_repr, general_repr):
     for i in prange(board_repr1.shape[0]):
@@ -242,7 +258,7 @@ def fast_normalise(board_repr1, board_repr2, board_vec, deck_repr, scores_repr, 
         board_vec[i, 1] /= 21.
         board_vec[i, 2:8] /= 21.
         board_vec[i, 8:8+6] /= 21.
-        board_vec[i, 8+6:8+18] /= 45.
+        board_vec[i, 8+6:8+12] /= 45.
         board_vec[i, 8+18:] /= 9.
 
         deck_repr[i] /= 4.
@@ -252,7 +268,6 @@ def fast_normalise(board_repr1, board_repr2, board_vec, deck_repr, scores_repr, 
 
     return (board_repr1, board_repr2, board_vec, deck_repr, scores_repr, general_repr)
 
-# @njit(cache=True)
 def fast_prepare_vector_input(board_vec_flat, deck_repr, scores_repr, general_repr_flat):
     b = board_vec_flat.shape[0]
     deck_repr_flat = deck_repr.reshape(b, -1)
@@ -261,13 +276,13 @@ def fast_prepare_vector_input(board_vec_flat, deck_repr, scores_repr, general_re
     vector_input = np.hstack((board_vec_flat, vector_for_grid))
     return vector_for_grid, vector_input
 
-# @njit(cache=True)
 def fast_prepare_grid_input(board_repr1, board_repr2):
     b = board_repr1.shape[0]
     board_repr2_reshaped = board_repr2.reshape(b, 11 + 2, 21 + 4, 4 * 6)
-    return np.concatenate((board_repr1, board_repr2_reshaped), axis=3)
+    combined = np.concatenate((board_repr1, board_repr2_reshaped), axis=3)
+    combined = np.transpose(combined, (0, 3, 1, 2))
+    return combined
 
-# @njit(cache=True)
 def fast_add_score_features(scores_repr):
     b = scores_repr.shape[0]
     scores_repr_updated = np.zeros((b, 3, 6))
@@ -275,7 +290,6 @@ def fast_add_score_features(scores_repr):
     scores_repr_updated[:, 2, :] = scores_repr[:, 0, :] - scores_repr[:, 1, :]
     return scores_repr_updated
 
-# @njit(cache=True)
 def fast_prepare(board_repr1, board_repr2, board_vec, deck_repr, scores_repr, general_repr):
     scores_repr = fast_add_score_features(scores_repr)
     vector_for_grid, vector_input = fast_prepare_vector_input(board_vec, deck_repr, scores_repr, general_repr)
@@ -284,6 +298,7 @@ def fast_prepare(board_repr1, board_repr2, board_vec, deck_repr, scores_repr, ge
 
 def fast_preprocess(inputs):
     inputs = fast_augment(*inputs)
+    # inputs = fast_random_transformations(*inputs)
     inputs = [input_.astype(np.float32) for input_ in inputs]
     inputs = fast_normalise(*inputs)
     inputs = fast_prepare(*inputs)

code/learn/strategy.py
@@ -3,11 +3,11 @@ from copy import deepcopy
 from numba import njit
 import numpy as np
 import torch
-import torch.nn as nn
 
 from game.board import combine_moves_and_deck
-from game.utils import get_other_player, find_winner_fast
+from game.game_utils import get_other_player, find_winner_fast
 from learn.network import get_network
+from learn.train_utils import set_model_to_half
 
 def get_strategy(strategy_type, params=None):
     if strategy_type == "random":
@@ -41,16 +41,9 @@ def get_strategy_types(params):
 
     return strategy_type_1, strategy_type_2
 
-def set_model_to_half(model):
-    model.half()
-    for layer in model.modules():
-        if isinstance(layer, nn.BatchNorm2d):
-            layer.float()
-
 def choose_random_move(board, deck):
     move_combinations = board.get_possible_moves()
     possible_moves = combine_moves_and_deck(move_combinations, deck.get_deck())
-    print(possible_moves.shape)
     random_idx = np.random.randint(0, high=possible_moves.shape[0] - 1)
     return possible_moves[random_idx]
 
@@ -63,9 +56,6 @@ def choose_max_scoring_move(board, deck, score):
     scores_diff = updated_scores - original_score
     total_scores_diff = np.sum(scores_diff, axis=1)
     max_index = np.argmax(total_scores_diff)
-
-    # board.batch_generate_state_representations(possible_moves)
-
     return possible_moves[max_index]
 
 def choose_increase_min_move(board, deck, score):

code/learn/train.py
@@ -1,6 +1,5 @@
 import os
 import time
-import json
 import argparse
 from itertools import product
 from shutil import copyfile
@@ -9,8 +8,8 @@ import numpy as np
 from tqdm import tqdm
 
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch import nn
+from torch import optim
 from torch.utils.tensorboard import SummaryWriter
 
 from utils.io import load_json, write_json, make_dir_if_not_exists
@@ -21,15 +20,7 @@ from learn.network import get_network
 from learn.replay_buffer import ReplayBuffer
 from learn.representation import RepresentationGenerator
 from learn.visualisation import generate_debug_visualisation
-
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-def set_model_to_half(model):
-    model.half()
-    for layer in model.modules():
-        if isinstance(layer, nn.BatchNorm2d):
-            layer.float()
+from learn.train_utils import set_model_to_half, set_optimizer_learning_rate
 
 class Params: 
     def __init__(self, d):
@@ -40,7 +31,7 @@ class Params:
         write_json(output_path, self.__dict__)
 
 class SelfPlayTrainingSession:
-    def __init__(self, args, config):
+    def __init__(self, config):
         self.config = config
         self.p = Params(self.config)
         self.game = get_gameplay(self.config)
@@ -70,16 +61,14 @@ class SelfPlayTrainingSession:
         self.net = self.get_new_network()
         print("Training using device:", self.device)
 
-        # self.p.initial_learning_rate = 0.1
         self.optimizer = optim.SGD(
             self.net.parameters(),
             lr=self.p.initial_learning_rate,
             momentum=0.9,
             weight_decay=self.p.weight_decay,
             )
+        self.lr_tracker = self.p.initial_learning_rate
         self.loss_criterion = nn.MSELoss(reduction='mean')
-
-        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, 1, gamma=0.1)
 
         if self.p.effective_batch_size % self.p.max_train_batch_size == 0:
             self.accumulate_loss_n_times = self.p.effective_batch_size // self.p.max_train_batch_size
@@ -124,7 +113,7 @@ class SelfPlayTrainingSession:
         self.steps_since_improvement = state["steps_since_improvement"]
         self.best_self_model_step = state["best_self_model_step"]
         self.best_rule_model_step = state["best_rule_model_step"]
-        # self.p.initial_learning_rate = state["learning_rate"]
+        self.lr_tracker = state["learning_rate"]
 
     def log_network_weights_hists(self, step):
         for name, params in self.net.named_parameters():
@@ -226,6 +215,29 @@ class SelfPlayTrainingSession:
         mean_abs_error = abs_error_sum / float(n)
         return p1_win_rate, avg_avg_loss, mean_abs_error
 
+    def step_learning_rate_scheduling(self, improved=True, elapsed_steps=0):
+        self.steps_since_improvement += elapsed_steps
+
+        if improved:
+            self.steps_since_improvement = 0
+            print("Model improved...")
+            if self.lr_tracker < self.p.restart_learning_rate:
+                self.lr_tracker = self.p.restart_learning_rate
+                print(f"Increasing learning rate to {self.lr_tracker}")
+                set_optimizer_learning_rate(self.optimizer, self.lr_tracker)
+        else:
+            if self.steps_since_improvement >= int(self.p.reduce_lr_step_threshold):
+                print(f"No improvement in {self.p.reduce_lr_step_threshold} steps...")
+                self.lr_tracker *= 0.1
+                print(f"Reducing learning rate to {self.lr_tracker}")
+                set_optimizer_learning_rate(self.optimizer, self.lr_tracker)
+
+        finish_training = False
+        if self.lr_tracker < self.p.lowest_learning_rate:
+            finish_training = True
+
+        return finish_training
+
     def add_graph_to_logs(self):
         inputs, _ = self.replay_buffer.sample_training_minibatch()
         grid_input_device = torch.tensor(inputs[0][0], dtype=torch.float16, device=self.device)
@@ -249,6 +261,7 @@ class SelfPlayTrainingSession:
             load_ckpt_path = os.path.join(self.p.restore_ckpt_dir, "latest.pth")
             self.load_model(load_ckpt_path)
             self.load_optimiser(self.p.restore_ckpt_dir)
+            set_optimizer_learning_rate(self.optimizer, self.lr_tracker)
 
         self.save_model(self.latest_ckpt_path)
 
@@ -273,6 +286,7 @@ class SelfPlayTrainingSession:
 
         running_loss, running_error = 0.0, 0.0
         best_win_rate_rule = 0.0
+        training_finished = False
         self.add_graph_to_logs()
 
         print("Start training")
@@ -299,11 +313,6 @@ class SelfPlayTrainingSession:
                 vis_figs = generate_debug_visualisation(vis_inputs)
                 self.writer.add_figure('examples', vis_figs, global_step=i)
 
-            if i > 0 and self.steps_since_improvement >= int(self.p.reduce_lr_step_threshold):
-                print("Reducing learning rate")
-                self.steps_since_improvement = 0
-                self.scheduler.step()
-
             if i % int(self.p.test_every_n_steps) == 0:
                 self.save_model(self.latest_ckpt_path)
                 copyfile(self.latest_ckpt_path, os.path.join(self.logs_dir, f"ckpt-{i}.pth"))
@@ -321,9 +330,11 @@ class SelfPlayTrainingSession:
                     self.training_p2.strategy.load_model(self.latest_ckpt_path)
                     copyfile(self.latest_ckpt_path, self.best_self_ckpt_path)
                     self.best_self_model_step = i
-                    self.steps_since_improvement = 0
+                    model_improved = True
                 else:
-                    self.steps_since_improvement += self.p.test_every_n_steps
+                    model_improved = False
+
+                training_finished = self.step_learning_rate_scheduling(improved=model_improved, elapsed_steps=self.p.test_every_n_steps)
 
                 win_rate_rule = 0.0
                 for strat in self.strategy_types:
@@ -341,6 +352,10 @@ class SelfPlayTrainingSession:
 
                 self.save_training_state()
 
+            if training_finished:
+                print(f"Training finished after {i} steps...")
+                break
+
         print(f"Final best self model was at step {self.best_self_model_step}")
         print(f"Final best rule model was at step {self.best_rule_model_step}")
         self.writer.close()
@@ -354,7 +369,7 @@ def parse_args():
 def main():
     args = parse_args()
     config = load_json(args.config_name)
-    SelfPlayTrainingSession(args, config).train()
+    SelfPlayTrainingSession(config).train()
 
 if __name__ == "__main__":
     main()

code/learn/visualisation.py
@@ -48,14 +48,9 @@ def split_vector_inputs(i, n):
 
     idx += 6
     score_counts = i[:, idx:idx+((3+8)*6)].reshape(n, 3 + 8, 6)
-    factors = np.array((21, 45, 45, 9, 9, 9, 9, 9, 9, 9, 9), dtype=np.float32)
+    factors = np.array((21, 45, 1, 9, 9, 9, 9, 9, 9, 9, 9), dtype=np.float32)
     score_counts *= factors.reshape(1, 3 + 8, 1)
     score_counts = score_counts.astype(np.int32)
-
-    # scoring_count = score_counts[:, 0, :]
-    # total_scores = score_counts[:, 1, :]
-    # non_scoring = score_counts[:, 2, :]
-    # top_scores = score_counts[:, 3:, :]
 
     idx += (3+8)*6
     deck_repr = i[:, idx:idx+(2*6)].reshape(n, 2, 6)
@@ -74,13 +69,14 @@ def split_vector_inputs(i, n):
 
 def split_inputs(inputs, n):
     (grid_inputs, _), vector_inputs = inputs
+    grid_inputs = np.transpose(grid_inputs, (0, 2, 3, 1)) # NCHW -> NHWC
     vis_grid_inputs = split_grid_inputs(grid_inputs[:n], n)
     vis_vector_inputs = split_vector_inputs(vector_inputs[:n], n)
     return vis_grid_inputs, vis_vector_inputs
 
 @njit(cache=True)
 def grid_to_coords(grid):
-    idxs = np.where(grid.astype(np.uint8) == 1)
+    idxs = np.where(grid.T.astype(np.uint8) == 1)
     num = idxs[0].shape[0]
 
     xs, ys = [], []
@@ -88,34 +84,10 @@ def grid_to_coords(grid):
         x = idxs[0][i]
         y = idxs[1][i]
 
-        if y % 2 == 0:
-            x += 0.5
-        xs.append(x)
+        xs.append(x / 2)
         ys.append(y)
 
     return (xs, ys)
-
-@njit(cache=True)
-def preproces_mask_total_score(max_total_score):
-    idxs = np.where(max_total_score.astype(np.int32) > 0)
-    num = idxs[0].shape[0]
-
-    max_total_score_vals = []
-    xs, ys = [], []
-    for i in range(num):
-        x = idxs[0][i]
-        y = idxs[1][i]
-
-        max_total_score_val = max_total_score[x, y]
-        max_total_score_vals.append(str(int(max_total_score_val)))
-
-        if y % 2 == 0:
-            x += 0.5
-        xs.append(x)
-        ys.append(y)
-
-    max_total_score_coords = (xs, ys)
-    return max_total_score_coords, max_total_score_vals
 
 def draw_coloured_numbers(numbers, start_x, start_y):
     colours = ["red", "darkorange", "gold", "forestgreen", "royalblue", "darkviolet"]
@@ -128,8 +100,11 @@ def generate_debug_visualisation(vis_inputs, num=4):
     inputs, labels, preds = vis_inputs
     vis_grid_inputs, vis_vector_inputs = split_inputs(inputs, num)
 
-    colour_states, _, available_grid, max_total_score = vis_grid_inputs
+    colour_states, _, available_grid, _ = vis_grid_inputs
     num_playable, num_available, colour_counts, score_counts, deck_repr, scores_repr, general_repr = vis_vector_inputs
+
+    colour_states = colour_states[:, 1:-1, 2:-2, :]
+    available_grid = available_grid[:, 1:-1, 2:-2]
 
     labels = labels.astype(np.float)
     labels[labels == 0] = -1
@@ -145,7 +120,6 @@ def generate_debug_visualisation(vis_inputs, num=4):
         blue = grid_to_coords(colour_states[i, :, :, 4])
         purple = grid_to_coords(colour_states[i, :, :, 5])
         available = grid_to_coords(available_grid[i])
-        # max_total_score_coords, max_total_score_vals = preproces_mask_total_score(max_total_score[i])
 
         your_score = scores_repr[i, 0, :].astype(np.int32)
         other_score = scores_repr[i, 1, :].astype(np.int32)
@@ -162,7 +136,6 @@ def generate_debug_visualisation(vis_inputs, num=4):
         plt.scatter(blue[0], blue[1], s=80, c='royalblue')
         plt.scatter(purple[0], purple[1], s=80, c='darkviolet')
         plt.scatter(available[0], available[1], facecolors='none', edgecolors='dimgrey')
-        # plt.annotate(max_total_score_vals, (max_total_score_coords[0], max_total_score_coords[1]), textcoords="offset points", xytext=(0, 0), ha='center')
         plt.axis('off')
 
         plt.subplot(num, 4, i * 4 + 2)
@@ -211,10 +184,10 @@ def generate_debug_visualisation(vis_inputs, num=4):
         font = {'family': 'serif', 'color':  'black', 'weight': 'normal', 'size': 12}
         plt.text(0, 1.0, f'Num playable:  {num_playable[i]}', fontdict=font)
         plt.text(0, 0.9, f'Num available: {num_available[i]}', fontdict=font)
-        plt.text(0, 0.8, f'Ingenious: {general_repr[i, 0]}', fontdict=font)
+        plt.text(0, 0.8, f'Ingenious:   {general_repr[i, 0]}', fontdict=font)
         plt.text(0, 0.7, f'# ingenious: {general_repr[i, 1]}', fontdict=font)
-        plt.text(0, 0.6, f'Can exchange:       {general_repr[i, 2]}', fontdict=font)
-        plt.text(0, 0.5, f'Should exchange:       {general_repr[i, 3]}', fontdict=font)
+        plt.text(0, 0.6, f'Can exchange:     {general_repr[i, 2]}', fontdict=font)
+        plt.text(0, 0.5, f'Should exchange:  {general_repr[i, 3]}', fontdict=font)
         plt.text(0, 0.4, f'Move #:      {general_repr[i, 4]}', fontdict=font)
         plt.text(0, 0.2, 'Win pred: {:.2f}'.format(preds[i]), fontdict=font)
         plt.text(0, 0.1, 'Win label: {:.2f}'.format(labels[i]), fontdict=font)